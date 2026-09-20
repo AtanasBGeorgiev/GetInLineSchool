81b4db9ddafe2c67074033b6d848a6c23a9ee4d3
AtanasBGeorgiev <atanas.b.georgiev@gmail.com>
2026-09-20 16:31:41 +0300

    Add files via upload

---
+++ GetInLineSchool/Services/SchoolService.cs
@@ -0,0 +1,34 @@
+﻿using GetInLineSchool.Repositories;
+using GetInLineSchool.Models;
+
+namespace GetInLineSchool.Services
+{
+    public class SchoolService
+    {
+        private readonly SchoolRepository _repository = new SchoolRepository();
+
+        public async Task<int> CreateSchoolAsync(School school)
+        {
+            if (await _repository.CheckUniqueFields(school) > 0)
+            {
+                return -1;
+            }
+
+            List<int> codes = await _repository.GetSchoolCodes();
+
+            int code = 0;
+
+            while (true)
+            {
+                code = HelperService.CreateSchoolCode();
+
+                if (!codes.Contains(code))
+                    break;
+            }
+
+            school.SchoolCode = code;
+
+            return await _repository.CreateSchoolAsync(school);
+        }
+    }
+}
---
+++ GetInLineSchool/Services/ServiceResult.cs
@@ -0,0 +1,34 @@
+﻿namespace GetInLineSchool.Services
+{
+    public class Error
+    {
+        public string Key { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
+    }
+    public class ServiceResult<T>
+    {
+        public bool IsSuccess { get; set; }
+        public T? Data { get; set; }
+        public List<Error>? Errors { get; set; }
+
+        public static ServiceResult<T>Success(T data)
+        {
+            return new ServiceResult<T>
+            {
+                IsSuccess = true,
+                Data = data,
+                Errors = null
+            };
+        }
+
+        public static ServiceResult<T>Failure(T data, List<Error> errors)
+        {
+            return new ServiceResult<T>
+            {
+                IsSuccess = false,
+                Data = data,
+                Errors = errors
+            };
+        }
+    }
+}
