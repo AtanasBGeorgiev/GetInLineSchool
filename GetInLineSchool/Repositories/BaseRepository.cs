297deeca6235c421abee4eadae8e6b7f738e72cf
AtanasBGeorgiev <atanas.b.georgiev@gmail.com>
2026-09-20 16:29:34 +0300

    Add files via upload

---
+++ GetInLineSchool/Repositories/BaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data.Common;
+
+namespace GetInLineSchool.Repositories
+{
+    public class BaseRepository
+    {
+        protected readonly string connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING") 
+            ?? throw new Exception("Connection string not found in environment variables.");
+
+        protected DbConnection dbConnection => new SqlConnection(connectionString);
+    }
+}
---
+++ GetInLineSchool/Repositories/SchoolRepository.cs
@@ -0,0 +1,39 @@
+﻿using Dapper;
+using GetInLineSchool.Models;
+using GetInLineSchool.Services;
+
+namespace GetInLineSchool.Repositories
+{
+    public class SchoolRepository : BaseRepository
+    {
+        public async Task<int> CreateSchoolAsync(School school)
+        {
+            using var connection = dbConnection;//automatically disposes the connection after the block is exited
+
+            var sql = @"
+                INSERT INTO Schools (Name, Address, City, Email, Phone, UnifiedIdentificationCode, MateriallyResponsiblePerson, SubscriptionPlan, IsPaid, EnableStudents, SchoolCode) 
+                VALUES (@Name, @Address, @City, @Email, @Phone, @UnifiedIdentificationCode, @MateriallyResponsiblePerson, @SubscriptionPlan, @IsPaid, @EnableStudents, @SchoolCode);";
+
+            return await connection.ExecuteAsync(sql, school);
+        }
+
+        public async Task<List<int>> GetSchoolCodes()
+        {
+            using var connection = dbConnection;
+
+            var result = await connection.QueryAsync<int>("SELECT SchoolCode FROM Schools");
+
+            return result.ToList();
+        }
+
+        public async Task<int> CheckUniqueFields(School school)
+        {
+            using var connection = dbConnection;
+
+            return await connection.QueryFirstOrDefaultAsync<int>
+                ("SELECT COUNT(*) FROM SCHOOLS WHERE Name=@Name OR Email=@Email OR Phone=@Phone OR " +
+                "UnifiedIdentificationCode=@UnifiedIdentificationCode", school);
+        }
+        
+    }
+}
