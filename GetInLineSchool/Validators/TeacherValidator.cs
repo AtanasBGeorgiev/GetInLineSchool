8af16f571572d1bce0f6e7e0c3c0f7f4ee471fd6
AtanasBGeorgiev <atanas.b.georgiev@gmail.com>
2026-09-21 18:52:58 +0300

    Add files via upload

---
+++ GetInLineSchool/Validators/TeacherValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using GetInLineSchool.Models;
+
+namespace GetInLineSchool.Validators
+{
+    public class TeacherValidator : AbstractValidator<Teacher>
+    {
+        public TeacherValidator()
+        {
+            RuleFor(t => t.Username)
+                .NotEmpty().WithMessage("Username is required.")
+                .Matches(@"^[А-ЯЁ]{2}\d{1,2}$")
+                .WithMessage("Username must have the format АБ1 or АБ10.");
+            RuleFor(t => t.Password)
+                .NotEmpty().WithMessage("Password is required.")
+                .Matches(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).{8,}$")
+                .WithMessage("Password must contain at least one lowercase letter, one uppercase letter, one digit, and one special character. Lenght 8+ symbols.");
+            RuleFor(t => t.Email)
+                .NotEmpty().WithMessage("Email is required.")
+                .EmailAddress().WithMessage("Invalid email format.");
+            RuleFor(t => t.IDSchool)
+                .NotEmpty().WithMessage("ID is required.");
+            RuleFor(t => t.Role)
+                .NotEmpty().WithMessage("Role is required.")
+                .InclusiveBetween(2,4).WithMessage("Invalid role value.");
+        }
+    }
+}
