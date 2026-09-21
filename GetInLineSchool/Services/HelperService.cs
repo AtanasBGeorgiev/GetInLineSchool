43d934a8285b5b8f07df5ec22126e6d56ca20a74
AtanasBGeorgiev <atanas.b.georgiev@gmail.com>
2026-09-21 18:52:29 +0300

    Add files via upload

--- GetInLineSchool/Services/HelperService.cs
+++ GetInLineSchool/Services/HelperService.cs
@@ -1,12 +1,18 @@
-﻿namespace GetInLineSchool.Services
+﻿using Isopoh.Cryptography.Argon2;
+
+namespace GetInLineSchool.Services
 {
     public class HelperService
     {
-        public static List<int> SchoolCodes = new List<int>();
         public static int CreateSchoolCode()
         {
             Random rnd = new Random();
-            return rnd.Next(000, 999);
+            return rnd.Next(100, 1000);
+        }
+
+        public static string HashPassword(string password)
+        {
+            return Argon2.Hash(password);
         }
     }
 }
---
+++ GetInLineSchool/Services/TeacherService.cs
@@ -0,0 +1,36 @@
+﻿using GetInLineSchool.Repositories;
+using GetInLineSchool.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.IdentityModel.Tokens;
+
+namespace GetInLineSchool.Services
+{
+    public class TeacherService
+    {
+        private readonly TeacherRepository _repository = new TeacherRepository();
+
+        public async Task<int> CreateTeacherAsync(Teacher teacher)
+        {
+            //if role=director check token role for admin
+
+            SchoolRepository schoolRepository = new SchoolRepository();
+            string schoolCode = await schoolRepository.GetCodeBySchoolAsync(teacher.IDSchool);
+
+            if (schoolCode == null)
+                return 0;
+
+            string username = schoolCode + teacher.Username;
+            teacher.Username = username;
+
+            var result = await _repository.CheckUniqueFieldsAsync(teacher);
+
+            if (result > 0)
+                return -1;
+
+            string hashedPassword = HelperService.HashPassword(teacher.Password);
+            teacher.Password = hashedPassword;
+
+            return await _repository.CreateTeacherAsync(teacher);
+        }
+    }
+}
